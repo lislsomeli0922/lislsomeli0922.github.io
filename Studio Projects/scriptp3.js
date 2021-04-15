bd7dd24b2487e269ca88d328eaa03b1004b14b4b
lislsomeli0922 <lomel918@newschool.edu>
2021-04-15 13:03:12 -0500

    project 3

---
+++ Studio Projects/scriptp3.js
@@ -0,0 +1,12 @@
+// function alert(h1) {
+// 	var txt;
+// 		if (confirm("After recieving a collection of 100 cats(!!!) I realized the impact that a cat image has had on the internet, especially for the creation of memes.")) {
+// 			txt = "Ok";
+// 		} else {
+// 			txt = "Cancel";
+// 		}
+// 		document.getElementById("demo").innerHTML = txt;
+// 	}
+
+
+alert("For this project I wanted to explore the relationship between the internet and cats! Why are cats such a meme? Is it their facial expressions or how their bodies look that make them so funny?? Through this website you are able to create your own meme and see if it works for you or not, your sense of humor dictates how the cat is seen and how a meme is finalized. Have fun :) Designed by Lissette Lomeli");
