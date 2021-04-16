affef1c0a27b41f92ee0ee0605b44e653de10221
lislsomeli0922 <lomel918@newschool.edu>
2021-04-15 19:11:12 -0500

    link to final

--- Studio Projects/scriptp3.js
+++ Studio Projects/scriptp3.js
@@ -1,12 +1,30 @@
-// function alert(h1) {
-// 	var txt;
-// 		if (confirm("After recieving a collection of 100 cats(!!!) I realized the impact that a cat image has had on the internet, especially for the creation of memes.")) {
-// 			txt = "Ok";
-// 		} else {
-// 			txt = "Cancel";
-// 		}
-// 		document.getElementById("demo").innerHTML = txt;
-// 	}
+function myFunction() {
+	var txt;
+		if (confirm("For this project I wanted to explore the relationship between the internet and cats! Why are cats such a meme? Is it their facial expressions or how their bodies look that make them so funny?? Through this website you are able to create your own meme and see if it works for you or not, your sense of humor dictates how the cat is seen and how a meme is finalized. Have fun :) Designed by Lissette Lomeli")) {
+			txt = "Ok";
+		} else {
+			txt = "Cancel";
+		}
+		document.getElementById("demo").innerHTML = txt;
+	}
 
 
-alert("For this project I wanted to explore the relationship between the internet and cats! Why are cats such a meme? Is it their facial expressions or how their bodies look that make them so funny?? Through this website you are able to create your own meme and see if it works for you or not, your sense of humor dictates how the cat is seen and how a meme is finalized. Have fun :) Designed by Lissette Lomeli");
+const allboxes = document.querySelectorAll(".box");
+const button2 = document.getElementById("button2");
+button2.addEventListener('click', showRandomBox)
+function showRandomBox() {
+  let randomNumber = Math.floor(Math.random() * allboxes.length); 
+  allboxes[randomNumber].classList.add('show');
+}
+
+const allcaptions = document.querySelectorAll(".caption");
+const button1 = document.getElementById("button1");
+button1.addEventListener('click', showRandomCaption)
+function showRandomCaption() {
+  let randomNumber = Math.floor(Math.random() * allcaptions.length); 
+  allcaptions[randomNumber].classList.add('show');
+}
+
+
+
+
